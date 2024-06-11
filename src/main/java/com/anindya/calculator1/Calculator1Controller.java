e9b224858b36b9a4a6d6ae6d1673b23b88909a1d
AnindyaFZ <anindyafarhan@gmail.com>
2024-06-11 21:29:03 +0600

    a comment added on line 10

--- src/main/java/com/anindya/calculator1/Calculator1Controller.java
+++ src/main/java/com/anindya/calculator1/Calculator1Controller.java
@@ -7,7 +7,7 @@ import javafx.scene.control.TextField;
 
 public class Calculator1Controller
 {
-
+/*here is the change*/
     @javafx.fxml.FXML
     private TextField textfieldone;
     @javafx.fxml.FXML
@@ -52,6 +52,51 @@ public class Calculator1Controller
             String numthree= Integer.toString(num3);
 
             answerlabel.setText(numthree);
+
+
+    }
+
+    @javafx.fxml.FXML
+    public void clearButtononaction(ActionEvent actionEvent) {
+        textfieldone.clear();
+        textfieldtwo.clear();
+    }
+
+    @javafx.fxml.FXML
+    public void multibuttonOnaction(ActionEvent actionEvent) {
+        String numOne = textfieldone.getText();
+        String numTwo = textfieldtwo.getText();
+
+        int num1= Integer.parseInt(numOne);
+        int num2= Integer.parseInt(numTwo);
+
+        int num3= num1*num2;
+        String numthree= Integer.toString(num3);
+
+        answerlabel.setText(numthree);
+
+
+
+
+    }
+
+    @javafx.fxml.FXML
+    public void divbuttonOnaction(ActionEvent actionEvent) {
+        String numOne = textfieldone.getText();
+        String numTwo = textfieldtwo.getText();
+
+        int num1= Integer.parseInt(numOne);
+        int num2= Integer.parseInt(numTwo);
+        if (num2==0){
+            answerlabel.setText("Cannot be divided by Zero :(");
+        }
+        else {
+            int num3= num1/num2;
+            String numthree= Integer.toString(num3);
+
+            answerlabel.setText(numthree);
+        }
+
 
 
     }
